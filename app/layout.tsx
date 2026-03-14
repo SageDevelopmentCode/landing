428b2383787eeb5bbbaa4b89f67b7eb63ed3513d
Julius <juliuscecilia33@gmail.com>
2026-03-13 23:52:02 -0500

    upated fonts

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,25 +1,19 @@
 import type { Metadata } from "next";
-import { Inter, Open_Sans, Merriweather } from "next/font/google";
+import { Merriweather, Poppins } from "next/font/google";
 import { Analytics } from "@vercel/analytics/next";
 import { SpeedInsights } from "@vercel/speed-insights/next";
 
 import "./globals.css";
 
-const inter = Inter({
-  variable: "--font-inter",
-  weight: ["400", "600", "700"],
-  subsets: ["latin"],
-});
-
-const openSans = Open_Sans({
-  variable: "--font-open-sans",
-  weight: ["400", "600", "700"],
-  subsets: ["latin"],
-});
-
 const merriweather = Merriweather({
   variable: "--font-merriweather",
   weight: ["300", "400", "700", "900"],
+  subsets: ["latin"],
+});
+
+const poppins = Poppins({
+  variable: "--font-poppins",
+  weight: ["400", "600", "700"],
   subsets: ["latin"],
 });
 
@@ -175,7 +169,7 @@ export default function RootLayout({
         />
       </head>
       <body
-        className={`${inter.variable} ${openSans.variable} ${merriweather.variable} antialiased`}
+        className={`${merriweather.variable} ${poppins.variable} antialiased`}
       >
         {children}
         <Analytics />
