ee78ede2219d9e4369f0368418edb722041eefd7
Julius <juliuscecilia33@gmail.com>
2026-03-18 00:41:24 -0500

    fb events

--- app/layout.tsx
+++ app/layout.tsx
@@ -167,10 +167,19 @@ export default function RootLayout({
           type="application/ld+json"
           dangerouslySetInnerHTML={{ __html: JSON.stringify(structuredData) }}
         />
+        {/* Meta Pixel */}
+        <script
+          dangerouslySetInnerHTML={{
+            __html: `!function(f,b,e,v,n,t,s){if(f.fbq)return;n=f.fbq=function(){n.callMethod?n.callMethod.apply(n,arguments):n.queue.push(arguments)};if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';n.queue=[];t=b.createElement(e);t.async=!0;t.src=v;s=b.getElementsByTagName(e)[0];s.parentNode.insertBefore(t,s)}(window,document,'script','https://connect.facebook.net/en_US/fbevents.js'); fbq('init', '2148097199288881'); fbq('track', 'PageView');`,
+          }}
+        />
       </head>
       <body
         className={`${merriweather.variable} ${poppins.variable} antialiased`}
       >
+        <noscript>
+          <img height="1" width="1" style={{ display: "none" }} src="https://www.facebook.com/tr?id=2148097199288881&ev=PageView&noscript=1" alt="" />
+        </noscript>
         {children}
         <Analytics />
         <SpeedInsights />
